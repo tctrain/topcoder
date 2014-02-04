d094f2ff98eb1bb8b6de7e65a6e9a400fb257b5a
Polkovnikov Philip <fpolkovnikov@pangenx.com>
2014-02-04 03:24:52 +0200

    TheEquation

--- polkovnikov.ph/TheEquation.cpp
+++ polkovnikov.ph/TheEquation.cpp
@@ -51,8 +51,8 @@ struct TheEquation {
 		fr(i, 2 * P + 1) {
 			fr(j, i) {
 				int k = i - j;
+				if (j == 0 || k == 0) continue;
 				if ((j * X + k * Y) % P == 0) {
-					cout << j << ' ' << k << endl;
 					return i;
 				}
 			}
