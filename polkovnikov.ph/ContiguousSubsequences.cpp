b623b60e6206636c1fa3659ace30bca3cc9e3fcd
Polkovnikov Philip <fpolkovnikov@pangenx.com>
2014-02-04 21:50:00 +0200

    ContiguousSubsequences

--- polkovnikov.ph/ContiguousSubsequences.cpp
+++ polkovnikov.ph/ContiguousSubsequences.cpp
@@ -52,7 +52,7 @@ struct ContiguousSubsequences {
 		int aii = 0, bii = 0;
 		ei(x, a) ei(y, a) if (ai < bi && bi - ai + 1 >= K) {
 			int n = 0;
-			for (int i = ai; i <= bi; ++j) {
+			for (int i = ai; i <= bi; ++i) {
 				n += a[i];
 			}
 			double f = double(n) / (bi - ai + 1);
