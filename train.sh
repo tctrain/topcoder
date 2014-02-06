acb07f8906eb60901682627884802dbf1f67da25
Polkovnikov Philip <fpolkovnikov@pangenx.com>
2014-02-06 22:47:13 +0200

    Training in-between train.sh starts added

--- train.sh
+++ train.sh
@@ -1,5 +1,6 @@
 BASEDIR=$(dirname $0)
 OTHER=$BASEDIR/polkovnikov.ph/
+(cd $BASEDIR; git add -A; git commit -m "Miscellaneous training"; git push)
 inotifywait $OTHER -r -m -e CLOSE_WRITE | while read dir events file; do
   if [[ $file =~ ^.*\.cpp$ ]] ; then
     (cd $BASEDIR; git add -A; git commit -m "${file%.cpp}"; git push)
