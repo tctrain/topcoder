17722a63406f5e6844ef0296a30a8e0547ba42ea
Polkovnikov Philip <fpolkovnikov@pangenx.com>
2014-02-23 22:56:32 +0200

    DeckRearranging

--- polkovnikov.ph/DeckRearranging.cpp
+++ polkovnikov.ph/DeckRearranging.cpp
@@ -50,7 +50,7 @@ struct DeckRearranging {
 	string rearrange(string deck, vector <int> above) {
 		string r;
 		ei(a, deck) {
-			r = r.substr(0, sz(r) - above[i]) + a + r.substr(sz(r) - above[i]);
+			r = r.substr(0, sz(r) - above[ai]) + a + r.substr(sz(r) - above[ai]);
 		}
 		return r;
 	}
