fc9dff6bdf2c2863dab23cb0673d7bc022b587c7
Polkovnikov Philip <fpolkovnikov@pangenx.com>
2014-02-23 23:22:55 +0200

    TheSimpleGame

--- polkovnikov.ph/TheSimpleGame.cpp
+++ polkovnikov.ph/TheSimpleGame.cpp
@@ -50,9 +50,7 @@ struct TheSimpleGame {
 	int count(int n, vector <int> x, vector <int> y) {
 		int c = 0;
 		ei(xx, x) {
-			int yy = y[xxi - 1];
-			cout << yy << ' ' << n - yy << endl;
-			cout << min(xx - 1, n - xx) << ' ' << min(yy - 1, n - yy) << endl;
+			int yy = y[xxi];
 			c += min(xx - 1, n - xx) + min(yy - 1, n - yy);
 		}
 		return c;
